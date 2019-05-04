fe0b40397190d97f76b0ce1099ce563cce56b871
Jose Cerqueira <jose@origensmedia.com>
2019-05-04 23:46:00 +0100

    Feat: Set up a  user saga

---
+++ src/sagas/users.js
@@ -0,0 +1,14 @@
+import { takeEvery } from "redux-saga/effects";
+import * as actions from "../actions/users";
+
+// Worker saga
+function* getUsers() {
+	try {
+	} catch (error) {}
+}
+
+// Watcher Saga
+// watch an action then calls a worker saga
+function* watchGetUsersRequest() {
+	yield takeEvery(actions.type.GET_USERS_REQUEST, getUsers);
+}
